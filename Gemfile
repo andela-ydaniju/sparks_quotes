861de9e04a48d1adcb72f9cc13a908d798eedbb0
andela-ydaniju <yusuf.daniju@andela.com>
2016-06-09 11:52:12 +0100

    finish body

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+gem 'rails', '>= 5.0.0.beta3', '< 5.1'
+gem 'puma'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks', '~> 5.x'
+gem 'jbuilder', '~> 2.0'
+gem 'rubocop'
+
+gem 'capistrano-rails', group: :development
+
+group :development, :test do
+  gem 'pry'
+  %w(rspec-core rspec-expectations rspec-mocks rspec-rails rspec-support).each do |lib|
+    gem lib, git: "https://github.com/rspec/#{lib}.git", branch: 'master'
+  end
+  gem 'sqlite3'
+end
+
+group :development do
+  gem 'web-console', '~> 3.0'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+gem 'pg', group: :production
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,50 @@
+class QuotesController < ApplicationController
+  def index
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to @quote
+    else
+      redirect_to new_quote_path
+    end
+  end
+
+  def edit
+    @quote = Quote.find_by(id: params[:id])
+  end
+
+  def update
+    @quote = Quote.find_by(id: params[:id])
+    if @quote.update_attributes(update_params)
+      redirect_to @quote
+    else
+      redirect_to edit_quote_path
+    end
+  end
+
+  def destroy
+    @quote = Quote.find_by(id: params[:id])
+    @quote.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:body, user_id: user.id)
+  end
+
+  def user
+    User.find_by(id: params[:id])
+  end
+
+  def update_params
+    params.require(:quote).permit(:body)
+  end
+end
